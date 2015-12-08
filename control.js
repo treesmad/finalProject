32d6f4e596864c8e601e934b9c5569860c78b4f6
Chelsea Parkkila <cmparkkila@gmail.com>
2015-12-08 12:07:18 -0500

    nav view and route

--- control.js
+++ control.js
@@ -1,2 +1,10 @@
 app.controller('aController', function($scope){
   });
+
+app.config('toHome', function($routeProvider){
+	$routeProvider.when("/app",
+	{
+		templateUrl: "app.html",
+		controller: "ctrlInput"
+	})
+});
---
+++ navController.js
@@ -0,0 +1,5 @@
+var navModule = angular.module('navModule', []);
+
+//this controller makes our nav links change the pageview
+
+navModule.controller('')
