13df404a44fe7c85e50152a9b9e5374b6497c976
treesmad <nmal@wayne.edu>
2015-12-10 10:33:02 -0500

    working user calander finally :^)

--- javascript/googauth.js
+++ javascript/googauth.js
@@ -1,9 +1,14 @@
+
 function logout(){
     gapi.auth.signOut();
     location.reload();
 }
 
 function login(){
+
+  gapi.client.setApiKey('AIzaSyCVggIYbIw4bkHOvunJj8Muwqf6gw9CrzQ');
+  gapi.client.load('plus', 'v1',function(){});
+
   var myParams = {
     'clientid' : '200772488238-3oe49iv349c5mj0g3avld1ch7igip7j5.apps.googleusercontent.com',
     'cookiepolicy' : 'single_host_origin',
@@ -17,16 +22,12 @@ function login(){
 
 
 function loginCallback(result){
+
     if(result['status']['signed_in']){
         var request = gapi.client.plus.people.get(
         {
             'userId': 'me'
-
-//loads homepage immediately on authorization
-//   gapi.auth.authorize(myParams, ‘future-ng-switch’)};)
-
-    });
-
+        });
 request.execute(function (resp){
     var email = '';
 if(resp['emails']){
@@ -44,8 +45,9 @@ str +=  email + "<br>";
 document.getElementById("profile").innerHTML = str;
 var link = "<a href='views/app.html'>Proceed To Home</a>"
 document.getElementById("home").innerHTML = link;
-var newMail = email.substr(0, email.length-10);
-document.getElementById("frame").setAttribute('src', "'https://calendar.google.com/calendar/embed?src='" + newMail + "'%40gmail.com&ctz=America/New_York'")
+$scope.newMail = email.substr(0, email.length-10);
+
+// document.getElementById("home").innerHTML = newLink;
     });
 }
     console.log(request);
